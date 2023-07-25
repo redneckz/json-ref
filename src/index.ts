a0ecc34000290e15ef4f377938936472b2214e16
redneckz <vasyaselsky@yandex.ru>
2023-07-25 16:01:18 +0300

    Add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export { resolveRef } from './resolveRef';
+export { visitRef, type RefVisitor } from './visitRef';
 
 export { resolveJPointer } from './resolveJPointer';
 
 export type { URIResolver } from './URIResolver';
 export { fetchResolver } from './fetchResolver';
 export { fileResolver } from './fileResolver';
+
+export * from './JSONNode';
